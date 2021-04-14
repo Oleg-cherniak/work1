b6798bdfed4f34b7258ed5ffb64ccf12e830f06f
Oleg-cherniak <70381411+Oleg-cherniak@users.noreply.github.com>
2021-04-14 17:02:19 +0300

    Update les2.js

--- les2.js
+++ les2.js
@@ -1,6 +1,9 @@
-const treug = (x, y) => {
-    const k = Math.pow(x, 2) + Math.pow(y, 2);
-    const gipot = Math.sqrt(k);
-    return gipot;
+const calculateHypotenuse = (leg1, leg2) => {
+    if (typeof leg1 !== "number" || typeof leg2 !== "number") return;
+
+    const legs = Math.pow(leg1, 2) + Math.pow(leg2, 2);
+    const hypotenuse = Math.sqrt(legs);
+    return hypotenuse;
 }
-console.log(treug(3, 4));
+
+console.log(calculateHypotenuse(3, 4));
