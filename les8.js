9670feafe0a6cc2258488be8045dfb20c0fc2b0f
Oleg-cherniak <70381411+Oleg-cherniak@users.noreply.github.com>
2021-04-14 17:35:23 +0300

    Update les8.js

--- les8.js
+++ les8.js
@@ -1,5 +1,7 @@
 const someFn = (array1, array2) => {
-    const array3 = array1.concat(array2);
+    if (!Array.isArray(array1) || !Array.isArray(array2)) return;
+
+    const array3 = [...array1, ...array2];
     return array3;
 }
 console.log(someFn([1,2],[3,4]));
